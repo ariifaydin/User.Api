57f2088fbe19771ebd6fa3c6c44423f808c01990
Arif Aydın <aydinariftr@gmail.com>
2023-12-17 12:35:50 +0300

    SAM-11 edited EFRepository.

--- User/User.Api/Repositories/EFRepository.cs
+++ User/User.Api/Repositories/EFRepository.cs
@@ -5,45 +5,42 @@ using User.Api.Entites;
 
 namespace User.Api.Repositories
 {
-    public class EFRepository<T, TContext> : IEFRepository<T>
-               where T : class
-            where TContext : DbContext, new()
+    public class EFRepository<TEntity> : IEFRepository<TEntity>
+               where TEntity : BaseEntity
     {
         private readonly DbContext _context;
-        private readonly DbSet<T> _dbSet;
+        private readonly DbSet<TEntity> _dbSet;
 
         public EFRepository(DbContext context)
         {
             _context = context;
-            _dbSet = _context.Set<T>();
+            _dbSet = _context.Set<TEntity>();
         }
 
-        public T GetById(int id)
+        public TEntity GetById(int id)
         {
             return _dbSet.Find(id);
         }
 
-        public IEnumerable<T> GetAll()
+        public IEnumerable<TEntity> GetAll()
         {
             return _dbSet.ToList();
         }
 
-        public void Add(T entity)
+        public void Add(TEntity entity)
         {
             _dbSet.Add(entity);
         }
 
-        public void Update(T entity)
+        public void Update(TEntity entity)
         {
             _context.Entry(entity).State = EntityState.Modified;
         }
 
-        public void Delete(T entity)
+        public void Delete(TEntity entity)
         {
             _dbSet.Remove(entity);
         }
-
-
     }
 }
 
